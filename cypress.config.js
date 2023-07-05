05144b03d21e6b6da29fceb3c2bdad015aabcba6
VladyslavDobrovolskyi <vladyslav.dobrovolski@gmail.com>
2023-07-05 18:38:52 +0300

    win setup

--- cypress.config.js
+++ cypress.config.js
@@ -1,13 +1,13 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
-    e2e: {
-        setupNodeEvents(on, config) {
-            // implement node event listeners here
-        },
-        supportFile: 'cypress/support/commands.js',
-        video: false,
-        browser: 'chrome',
-        baseUrl: 'http://172.29.194.13:3000',
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
     },
+    supportFile: 'cypress/support/commands.js',
+    video: false,
+    browser: 'chrome',
+    baseUrl: 'http://localhost:3000',
+  },
 })
